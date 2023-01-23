b0c6b3396d6d48e2056a910d02efc3ef97c8e81c
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-01-23 16:22:38 +0530

    TF Base Code

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "awsb42practice"
-    key            = "terraform.tfstate"
+    key            = "dependson-terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "devopsb27-terraform-locking"
     encrypt        = true
--- vpc.tf
+++ vpc.tf
@@ -6,6 +6,10 @@ resource "aws_vpc" "testvpc01" {
     Owner       = "Rajasekhar"
     environment = var.env
   }
+  depends_on = [
+    aws_s3_bucket.vpcflowlogsbucket
+  ]
+
 }
 resource "aws_internet_gateway" "testvpc01" {
   vpc_id = aws_vpc.testvpc01.id
