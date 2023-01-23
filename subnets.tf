a5a0d4accddff183919e7ce04c33ff93e037d18f
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-01-23 11:28:05 +0530

    TF Base Code

--- subnets.tf
+++ subnets.tf
@@ -1,34 +1,39 @@
 resource "aws_subnet" "subnet1-public" {
-  vpc_id            = aws_vpc.testvpc01.id
-  cidr_block        = var.public_subnet1_cidr
-  availability_zone = "us-east-2a"
+ vpc_id            = aws_vpc.testvpc01.id
+ cidr_block        = var.public_subnet1_cidr
+ availability_zone = "us-west-2a"
 
 
-  tags = {
-    Name = "${var.vpc_name}-subnet-1"
-  }
+ tags = {
+   Name = "${var.vpc_name}-Public-Subnet-1"
+   Owner = "DevOpsB27"
+ }
 }
 
 
 resource "aws_subnet" "subnet2-public" {
-  vpc_id            = aws_vpc.testvpc01.id
-  cidr_block        = var.public_subnet2_cidr
-  availability_zone = "us-east-2b"
+ vpc_id            = aws_vpc.testvpc01.id
+ cidr_block        = var.public_subnet2_cidr
+ availability_zone = "us-west-2b"
 
 
-  tags = {
-    Name = "${var.vpc_name}-subnet-2"
-  }
+ tags = {
+   Name = "${var.vpc_name}-Public-Subnet-2"
+   Owner = "DevOpsB27"
+ }
 }
 
 
 resource "aws_subnet" "subnet3-public" {
-  vpc_id            = aws_vpc.testvpc01.id
-  cidr_block        = var.public_subnet3_cidr
-  availability_zone = "us-east-2c"
+ vpc_id            = aws_vpc.testvpc01.id
+ cidr_block        = var.public_subnet3_cidr
+ availability_zone = "us-west-2c"
 
 
-  tags = {
-    Name = "${var.vpc_name}-subnet-3"
-  }
+ tags = {
+   Name = "${var.vpc_name}-Public-Subnet-3"
+   Owner = "DevOpsB27"
+ }
+
+
 }
