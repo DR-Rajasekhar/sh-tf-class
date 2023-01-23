75c7a4f43fa160eaf7b810382bdbccffba434396
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-01-23 11:51:19 +0530

    Reading Outputs

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "vpc_id" {
+  value     = aws_vpc.testvpc01.id
+  sensitive = false
+}
+output "vpc_arn" {
+  value     = aws_vpc.testvpc01.arn
+  sensitive = false
+}
--- subnets.tf
+++ subnets.tf
@@ -1,39 +1,39 @@
 resource "aws_subnet" "subnet1-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet1_cidr
- availability_zone = "us-west-2a"
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-west-2a"
 
 
- tags = {
-   Name = "${var.vpc_name}-Public-Subnet-1"
-   Owner = "DevOpsB27"
- }
+  tags = {
+    Name  = "${var.vpc_name}-Public-Subnet-1"
+    Owner = "DevOpsB27"
+  }
 }
 
 
 resource "aws_subnet" "subnet2-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet2_cidr
- availability_zone = "us-west-2b"
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-west-2b"
 
 
- tags = {
-   Name = "${var.vpc_name}-Public-Subnet-2"
-   Owner = "DevOpsB27"
- }
+  tags = {
+    Name  = "${var.vpc_name}-Public-Subnet-2"
+    Owner = "DevOpsB27"
+  }
 }
 
 
 resource "aws_subnet" "subnet3-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet3_cidr
- availability_zone = "us-west-2c"
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "us-west-2c"
 
 
- tags = {
-   Name = "${var.vpc_name}-Public-Subnet-3"
-   Owner = "DevOpsB27"
- }
+  tags = {
+    Name  = "${var.vpc_name}-Public-Subnet-3"
+    Owner = "DevOpsB27"
+  }
 
 
 }
